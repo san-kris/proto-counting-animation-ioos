c1a6a84853fb3108478862b236afa6fadaba06be
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-16 16:03:07 -0500

    Initial Commit

---
+++ proto-counting-animation-ioos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-counting-animation-ioos
+//
+//  Created by Santosh Krishnamurthy on 3/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
